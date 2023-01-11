947044f5ad9798b94c338b26654dbd7bc61620de
OualidZejli <113343262+OualidZejli@users.noreply.github.com>
2023-01-11 14:36:39 +0100

    JS 1

--- JavaScript/TUTO/js/App.js
+++ JavaScript/TUTO/js/App.js
@@ -1,1 +1,23 @@
-prompt("Comment ça va ?");
+/*let maVariable = 10;
+let typeOfMaVariable = typeof maVariable;
+alert(typeOfMaVariable);*/
+
+// let humeur = prompt('Comment ça va ?');
+// let prenom = prompt("Comment t'appeles-tu ?");
+// let phrase = 'Tu t\'appelles ' + prenom + ' et tu vas ' + humeur + ' !';
+// alert(phrase);
+
+console.log('Affichage du message de bienvenue');
+alert('Bienvenue dans notre calculatrice !');
+
+let nombreUn = Number(prompt('Votre premier nombre'));
+console.log('Le premier nombre est ' + nombreUn);
+
+let nombreDeux = Number(prompt('Votre deuxième nombre'));
+console.log('Le premier nombre est ' + nombreDeux);
+
+console.log('Opération en cours');
+var resultat = nombreUn + nombreDeux;
+
+console.log('Le  résultat est ' + resultat);
+alert(resultat);
